c0c9d68d92084d344f9e33ec97d647339ef86114
Santacruz Avalos Ailyn <168689298+ailyn27sa@users.noreply.github.com>
2024-06-10 10:39:08 -0600

    Add files via upload

---
+++ ejercicio54.cpp
@@ -0,0 +1,3 @@
+ // Ailyn Inciso d: Elevar el número natural a la potencia de los decimales
+    double potencia = pow(numero_entero, numero - numero_entero);
+    std::cout << "d) " << numero_entero << " elevado a la potencia de los decimales es: " << potencia << std::endl;
